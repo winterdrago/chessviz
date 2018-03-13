7d60b942f29ee252049f4cb6e15c82a5397e5623
Винтер Антон  Вячеславович <ip715s03@mwp04.csc.local>
2018-03-13 15:02:48 +0700

    Create chess

--- chess.c
+++ chess.c
@@ -2,5 +2,5 @@
 
 int main()
 {
-printf("8 L H E K Q E H L\n 7 S S S S S S S S\n 6\n 5\n 4\n 3\n 2 S S S S S S S S\n 1 L H E K Q E H L\n  A B C D E F G H\n");
+printf("\n 8 L H E K Q E H L\n 7 S S S S S S S S\n 6\n 5\n 4\n 3\n 2 s s s s s s s s\n 1 l h e k q e h l\n   A B C D E F G H\n");
 }
