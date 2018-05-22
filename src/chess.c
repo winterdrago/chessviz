e37f7427e21b18ca0b1beb29f6ee1c7d2c70f786
Винтер Антон  Вячеславович <ip715s03@mwp07.csc.local>
2018-05-22 13:44:50 +0700

    better board

--- src/chess.c
+++ src/chess.c
@@ -1,15 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char **board;
+
+void fill_board()
+{
+    int i;
+    for (i = 0; i < 8; i++){
+        board[i][1] = 'P';
+        board[i][6] = 'p';
+    }
+    board[0][0] = 'R';
+    board[1][0] = 'N';
+    board[2][0] = 'B';
+    board[3][0] = 'Q';
+    board[4][0] = 'K';
+    board[5][0] = 'B';
+    board[6][0] = 'N';
+    board[7][0] = 'R';
+    board[0][7] = 'r';
+    board[1][7] = 'n';
+    board[2][7] = 'b';
+    board[3][7] = 'q';
+    board[4][7] = 'k';
+    board[5][7] = 'b';
+    board[6][7] = 'n';
+    board[7][7] = 'r';
+}
+
+void clear_board()
+{
+    int i;
+    for (i = 0; i < 8; i++) {
+        free(board[i]);
+    }
+    free(board);
+    board = NULL;
+}
+
+void print_board()
+{
+    int i,j;
+    for (i = 7; i >= -1; i--) {
+        printf("  ---------------------------------\n");
+        for (j = 8; j > -1; j--) {
+            if (j > 7 && i < 0) {
+                printf("   ");
+            } else  if (j > 7) {
+                printf("%d |",i + 1);
+            } else if (i < 0) {
+                printf(" %c  ",'H' - j);
+            } else {
+                printf(" %c |",board[7-j][i]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void init_board()
+{
+    int i,j;
+    board = (char**)malloc(8 * sizeof(char*));
+    for (i = 0; i < 8; i++) {
+        board[i] = (char*)malloc(8 * sizeof(char));
+        for (j = 0; j < 8; j++) {
+            board[i][j] = ' ';
+        }
+    }
+    fill_board();
+}
 
 int main()
 {
-	printf("\
-	8 R N B K Q B N R\n\
-	7 P P P P P P P P\n\
-	6 \n\
-	5 \n\
-	4 \n\
-	3 \n\
-	2 p p p p p p p p\n\
-	1 r n b k q b n r\n");
-	return 0;
+    init_board();
+    print_board();
+    clear_board();
+    return 0;
 }
