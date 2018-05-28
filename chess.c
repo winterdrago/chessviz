c52ea7ad24e57402f54ba691d4011a0b70fc1542
winterdrago <36185869+winterdrago@users.noreply.github.com>
2018-05-28 20:27:07 +0700

    Delete chess.c

--- chess.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-# include <conio.h>
-main()
-{ 
-	printf("8 R N B K Q B N R \n7 P P P P P P P P \n6 \n5 \n4 \n3 \n2 P P P P P P P P \n1 R N B K Q B N R \n
-  a b c d e f g h");
-getch();
-} 
